bc70552a10d35e6ad2ca7aae870f551feb0e9df8
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-08-08 15:43:24 +0300

    Final edits and additions have been made.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}", 
+    "../home/app/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
